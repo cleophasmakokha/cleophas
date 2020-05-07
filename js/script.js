550e08493de10632cbf25f2bbb08cb7bc4eaa140
cleophas <makokhacleophas@gmail.com>
2020-05-08 02:11:51 +0300

    Adds a function to verify name on the js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 var currentDate = new Date();
 var currentYear = currentDate.getFullYear();
+
+function giveName() {
+  var name = document.getElementById("yourNames").value;
+  if (name == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
