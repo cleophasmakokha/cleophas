087fd7663f6a7173cf3be1349cb1c2a6ca263381
cleophas <makokhacleophas@gmail.com>
2020-05-08 02:36:48 +0300

    Adds a function that parses variables and returns a  specific gender

--- js/script.js
+++ js/script.js
@@ -94,3 +94,15 @@ function validDay(){
     return false;
   }
 }
+
+function getName(){
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+  var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+  var cc = parseInt(document.getElementById("centuryBorn").value.slice(0, 2));
+  var yy = parseInt(document.getElementById("yearBorn").value.slice(2, 4));
+  var dd = parseInt(document.getElementById("dayBorn").value);
+  var mm = parseInt(document.getElementById("monthBorn").value);
+  var day = (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7;
+  var akanDay = days[day.toFixed()];
+  var gender = parseInt(document.getElementById("gender").value);
